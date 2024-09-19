609078400595a0d521302f2d1b44b226c392f7d6
Aman Manwani <aman04.manwani@gmail.com>
2024-09-20 04:44:23 +0530

    start adding compiler

--- Client/app/(auth)/layout.tsx
+++ Client/app/(auth)/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { dark } from "@clerk/themes";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
       <html lang="en">
-        <body className={`${inter.className} h-screen bg-[rgb(17,24,39)] flex justify-center items-center`}>{children}</body>
+        <body className={`${inter.className} h-screen bg-[rgb(0,0,0)] flex justify-center items-center`}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- Client/app/(auth)/onboarding/page.tsx
+++ Client/app/(auth)/onboarding/page.tsx
@@ -1,10 +1,18 @@
+import AccountProfile from '@/components/shared/AccountProfile'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="mx-auto flex flex-col sm:w-full lg:w-2/4 justify-start sm:px-5 py-20">
+      <h1 className="font-bold text-3xl text-white">OnBoarding</h1>
+      <p className="mt-5 text-base-regular text-white">
+        Complete your profile now to use AI-Compiler
+      </p>
+
+      <section className="mt-3 bg-dark-2 py-10 ">
+        <AccountProfile />
+      </section>
+    </main>
   )
 }
 
