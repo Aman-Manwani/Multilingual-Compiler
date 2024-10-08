f69e5193133fbc28e989e7a63202f1e6afdda25f
Aman Manwani <aman04.manwani@gmail.com>
2024-10-08 16:35:03 +0530

    added multiple languages

--- Client/constants/languages.ts
+++ Client/constants/languages.ts
@@ -5,6 +5,10 @@ export const LANGUAGE_VERSIONS: Record<string, string> = {
     java: "15.0.2",
     csharp: "6.12.0",
     php: "8.2.3",
+    cpp: "10.2.0",
+    kotlin: "1.5.31",
+    bash: "5.1.0",   
+    go: "1.23.0",
   };
   
   export const CODE_SNIPPETS: Record<string, string> = {
@@ -14,5 +18,9 @@ export const LANGUAGE_VERSIONS: Record<string, string> = {
     java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
     csharp: `using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n`,
     php: `<?php\n\n$name = 'Alex';\necho $name;\n`,
+    cpp: `#include <iostream>\n\nint main() {\n\tstd::cout << "Hello, World!";\n\treturn 0;\n}\n`,
+    kotlin: `fun main() {\n\tprintln("Hello, World!")\n}\n`,
+    bash: `echo "Hello, World!"\n`,
+    go: `package main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello, World!")\n}\n`,
   };
   
