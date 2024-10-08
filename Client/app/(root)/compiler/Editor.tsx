99be78e62563c817e0590f0bb4cd402e63c12026
Aman Manwani <aman04.manwani@gmail.com>
2024-10-08 19:31:22 +0530

    added themes

--- Client/app/(root)/compiler/Editor.tsx
+++ Client/app/(root)/compiler/Editor.tsx
@@ -1,55 +1,73 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { useRef, useState } from "react";
 import { Editor, Monaco } from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../../../constants/languages";
 import Output from "./Output";
+import { MoonIcon } from "@heroicons/react/24/solid";
+import { SunIcon } from "@heroicons/react/20/solid";
 
 const EditorComponent = () => {
-    const editorRef = useRef<Monaco | null>(null);
-    const [value, setValue] = useState<string>("");
-    const [language, setLanguage] = useState<string>("javascript");
-  
-    const onMount = (editor: Monaco) => {
-      editorRef.current = editor;
-      editor.focus();
-    };
-  
-    const onSelect = (language: string) => {
-      setLanguage(language);
-      setValue(CODE_SNIPPETS[language]);
-    };
-  
-  return (
-      <div className="px-4 pt-4 flex space-x-4">
-        <div className="w-full">
-          <div className="flex justify-between">
-            <LanguageSelector language={language} onSelect={onSelect} />
-            <div className="flex items-end justify-between pr-3 py-5">
-              <button className="rounded-md bg-white text-black hover:bg-gray-800 hover:text-white px-4 py-3">Publish Your Code</button>
-            </div>
-          </div>
-          <Editor
-            options={{
-              minimap: {
-                enabled: true,
-              },
-            }}
-            height="75vh"
-            theme="vs-dark"
-            language={language}
-            defaultValue={CODE_SNIPPETS[language]}
-            onMount={onMount}
-            value={value}   
-            onChange={(value) => setValue(value || "")}
-          />
-        </div>
-        <div className="w-full">
-          <Output editorRef={editorRef} language={language} />
-        </div>
-      </div>
-  )
-}
+  const editorRef = useRef<Monaco | null>(null);
+  const [value, setValue] = useState<string>("");
+  const [language, setLanguage] = useState<string>("javascript");
+  const [isDark, setIsDark] = useState<boolean>(true);
 
-export default EditorComponent
+  const onMount = (editor: Monaco) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+
+  const onSelect = (language: string) => {
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+  };
+
+  return (
+    <div className="px-4 pt-4 flex space-x-4">
+      <div className="w-full">
+        <div className="flex justify-between">
+          <LanguageSelector language={language} onSelect={onSelect} />
+          <div className="flex items-end justify-between pr-3 py-5">
+            <button 
+                className="flex gap-3 rounded-md bg-gray-800 text-white font-semibold hover:bg-gray-600 px-4 py-3"
+                onClick={() => setIsDark(!isDark)}
+            >
+              <p>{isDark === true ? "Dark" : "Light"}</p>
+              {isDark === true ? (
+                <MoonIcon className="h-6 w-6" />
+              ) : (
+                <SunIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+          <div className="flex items-end justify-between pr-3 py-5">
+            <button className="rounded-md bg-gray-800 text-white font-semibold hover:bg-gray-600 px-4 py-3">
+              Publish Your Code
+            </button>
+          </div>
+        </div>
+        <Editor
+          options={{
+            minimap: {
+              enabled: true,
+            },
+          }}
+          height="75vh"
+          theme={isDark === true ? "vs-dark" : "light"}
+          language={language}
+          defaultValue={CODE_SNIPPETS[language]}
+          onMount={onMount}
+          value={value}
+          onChange={(value) => setValue(value || "")}
+        />
+      </div>
+      <div className="w-full">
+        <Output editorRef={editorRef} language={language} />
+      </div>
+    </div>
+  );
+};
+
+export default EditorComponent;
--- Client/constants/languages.ts
+++ Client/constants/languages.ts
@@ -9,18 +9,18 @@ export const LANGUAGE_VERSIONS: Record<string, string> = {
     kotlin: "1.5.31",
     bash: "5.1.0",   
     go: "1.23.0",
-  };
+};
   
-  export const CODE_SNIPPETS: Record<string, string> = {
+export const CODE_SNIPPETS: Record<string, string> = {
     javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
     typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
     python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
     java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
     csharp: `using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n`,
-    php: `<?php\n\n$name = 'Alex';\necho $name;\n`,
-    cpp: `#include <iostream>\n\nint main() {\n\tstd::cout << "Hello, World!";\n\treturn 0;\n}\n`,
-    kotlin: `fun main() {\n\tprintln("Hello, World!")\n}\n`,
-    bash: `echo "Hello, World!"\n`,
-    go: `package main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello, World!")\n}\n`,
-  };
+    php: `\n<?php\n\n$name = 'Alex';\necho $name;\n`,
+    cpp: `\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello, World!";\n\treturn 0;\n}\n`,
+    kotlin: `\nfun main() {\n\tprintln("Hello, World!")\n}\n`,
+    bash: `\necho "Hello, World!"\n`,
+    go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello, World!")\n}\n`,
+};
   
